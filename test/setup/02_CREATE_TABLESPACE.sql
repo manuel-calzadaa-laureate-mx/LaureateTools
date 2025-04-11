0a127ddfab8814e1075cc993f4479c52dd8612af
Alejandro Calzada <alejandro.calzada.algravez@gmail.com>
2025-04-11 16:01:53 -0600

    improve pre-setup files

--- test/setup/02_CREATE_TABLESPACE.sql
+++ test/setup/02_CREATE_TABLESPACE.sql
@@ -1,11 +1,37 @@
+-- 1. Create DEVELOPMENT tablespace
 CREATE TABLESPACE DEVELOPMENT
-  DATAFILE '/tmp/oracle_datafiles/development01.dbf'  -- Ephemeral datafile location
-  SIZE 100M                                           -- Initial size of the datafile
-  AUTOEXTEND ON                                       -- Enable auto-extend
-  NEXT 10M                                            -- Size of the next extension
-  MAXSIZE UNLIMITED                                   -- Maximum size of the datafile
-  EXTENT MANAGEMENT LOCAL                             -- Use local extent management
-  SEGMENT SPACE MANAGEMENT AUTO                       -- Use automatic segment space management
-  LOGGING;                                            -- Enable logging for the tablespace
+  DATAFILE '/opt/oracle/oradata/FREE/development01.dbf'
+  SIZE 100M
+  AUTOEXTEND ON NEXT 10M
+  MAXSIZE 2G
+  EXTENT MANAGEMENT LOCAL
+  SEGMENT SPACE MANAGEMENT AUTO
+  ONLINE
+  NOLOGGING;
 
-  PROMPT Tablespace created successfully
+-- 2. Create TEMP tablespace (if needed)
+CREATE TEMPORARY TABLESPACE TEMP_DEV
+  TEMPFILE '/opt/oracle/oradata/FREE/temp_dev01.dbf'
+  SIZE 50M
+  AUTOEXTEND ON NEXT 5M
+  MAXSIZE 1G;
+
+-- 3. Create UNDO tablespace (optional, only if default is insufficient)
+BEGIN
+  EXECUTE IMMEDIATE 'CREATE UNDO TABLESPACE UNDO_DEV
+    DATAFILE ''/opt/oracle/oradata/FREE/undo_dev01.dbf''
+    SIZE 100M
+    AUTOEXTEND ON NEXT 10M
+    MAXSIZE 2G';
+EXCEPTION
+  WHEN OTHERS THEN
+    DBMS_OUTPUT.PUT_LINE('Note: Could not create undo tablespace. Using default: ' || SQLERRM);
+END;
+/
+
+-- 4. Verify tablespaces were created
+SELECT tablespace_name, status, contents, logging
+FROM dba_tablespaces
+WHERE tablespace_name IN ('DEVELOPMENT', 'TEMP_DEV', 'UNDO_DEV');
+
+PROMPT Tablespaces created successfully in /opt/oracle/oradata/FREE/;
--- test/setup/03_CREATE_PACKAGE_GFKSJPA.sql
+++ test/setup/03_CREATE_PACKAGE_GFKSJPA.sql
@@ -1,48 +1,49 @@
 -- Package Specification
 CREATE OR REPLACE PACKAGE GFKSJPA AS
-  -- Global variables
-  ID NUMBER;
-  VERSION NUMBER;
-
-  -- Functions
+  -- Declare functions/procedures (but NOT variables here)
   FUNCTION GETID RETURN NUMBER;
   FUNCTION GETVERSION RETURN NUMBER;
 
-  -- Procedures
   PROCEDURE SETID(p_id IN NUMBER);
   PROCEDURE SETVERSION(p_version IN NUMBER);
 END GFKSJPA;
 /
 SHOW ERRORS PACKAGE GFKSJPA;
 
--- Package Body
+-- Package Body (with variables properly declared)
 CREATE OR REPLACE PACKAGE BODY GFKSJPA AS
-  FUNCTION GETID return number IS
-  BEGIN
-    RETURN 0;
-  END;
+  -- Global variables MUST be declared in the body
+  v_id NUMBER := 0;       -- Default value
+  v_version NUMBER := 0;  -- Default value
 
-  FUNCTION GETVERSION return number IS
+  -- Implement functions
+  FUNCTION GETID RETURN NUMBER IS
   BEGIN
-    RETURN 0;
-  END;
+    RETURN v_id;
+  END GETID;
 
+  FUNCTION GETVERSION RETURN NUMBER IS
+  BEGIN
+    RETURN v_version;
+  END GETVERSION;
+
+  -- Implement procedures
   PROCEDURE SETID(p_id IN NUMBER) IS
   BEGIN
-    ID := p_id;
-  END;
+    v_id := p_id;
+  END SETID;
 
   PROCEDURE SETVERSION(p_version IN NUMBER) IS
   BEGIN
-    VERSION := p_version;
-  END;
+    v_version := p_version;
+  END SETVERSION;
 
 END GFKSJPA;
 /
 SHOW ERRORS PACKAGE BODY GFKSJPA;
 
+-- Create a public synonym (optional but useful)
 CREATE OR REPLACE PUBLIC SYNONYM GFKSJPA FOR GFKSJPA;
 GRANT EXECUTE ON GFKSJPA TO PUBLIC;
 
--- Print a success message
-PROMPT Package GFKSJPA created successfully.
+PROMPT Package GFKSJPA created successfully. Ready for use.;
